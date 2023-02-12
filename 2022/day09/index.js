d5ae4855e9b3e93da0652c119eef0e6689ccde92
Alejandro Villanueva <aavillanueva6@gmail.com>
2023-02-11 22:43:46 -0800

    day 9 part 2 solution

--- 2022/day09/index.js
+++ 2022/day09/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 let input = fs.readFileSync('./input.txt', 'utf8').split('\n');
 
-// input = ['R 4', 'U 4', 'L 3', 'D 1', 'R 4', 'D 1', 'L 5', 'R 2']; // 13
+// input = ['R 4', 'U 4', 'L 3', 'D 1', 'R 4', 'D 1', 'L 5', 'R 2']; // 13 , 1 (pt1, pt2)
+// input = ['R 5', 'U 8', 'L 8', 'D 3', 'R 17', 'D 10', 'L 25', 'U 20']; // 36 (pt2 only)
 
 let parsedInput = [];
 input.forEach((element) => {
@@ -65,4 +66,79 @@ for (let i = 0; i < parsedInput.length; i++) {
 console.log(tSet.size);
 
 // part 2
-let nineSet = new Set();
+
+let locations = [
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+  [0, 0],
+];
+
+let t9LocSet = new Set();
+t9LocSet.add(`${locations[9][0]},${locations[9][1]}`);
+
+function moveKnot(k) {
+  if (locations[k - 1][0] === locations[k][0]) {
+    if (locations[k - 1][1] > locations[k][1])
+      locations[k][1] = locations[k][1] + 1;
+    else locations[k][1] = locations[k][1] - 1;
+  } else if (locations[k - 1][1] === locations[k][1]) {
+    if (locations[k - 1][0] > locations[k][0])
+      locations[k][0] = locations[k][0] + 1;
+    else locations[k][0] = locations[k][0] - 1;
+  } else {
+    if (
+      locations[k - 1][0] > locations[k][0] &&
+      locations[k - 1][1] > locations[k][1]
+    ) {
+      locations[k][0] = locations[k][0] + 1;
+      locations[k][1] = locations[k][1] + 1;
+    } else if (
+      locations[k - 1][0] < locations[k][0] &&
+      locations[k - 1][1] < locations[k][1]
+    ) {
+      locations[k][0] = locations[k][0] - 1;
+      locations[k][1] = locations[k][1] - 1;
+    } else if (locations[k - 1][0] > locations[k][0]) {
+      locations[k][0] = locations[k][0] + 1;
+      locations[k][1] = locations[k][1] - 1;
+    } else {
+      locations[k][0] = locations[k][0] - 1;
+      locations[k][1] = locations[k][1] + 1;
+    }
+  }
+}
+
+for (let i = 0; i < parsedInput.length; i++) {
+  for (let j = 0; j < parseInt(parsedInput[i][1]); j++) {
+    if (parsedInput[i][0] === 'R') {
+      locations[0][0] = locations[0][0] + 1;
+    } else if (parsedInput[i][0] === 'L') {
+      locations[0][0] = locations[0][0] - 1;
+    } else if (parsedInput[i][0] === 'U') {
+      locations[0][1] = locations[0][1] + 1;
+    } else {
+      locations[0][1] = locations[0][1] - 1;
+    }
+    for (let k = 1; k < locations.length; k++) {
+      dist = Math.sqrt(
+        (locations[k - 1][0] - locations[k][0]) *
+          (locations[k - 1][0] - locations[k][0]) +
+          (locations[k - 1][1] - locations[k][1]) *
+            (locations[k - 1][1] - locations[k][1])
+      );
+      if (dist > 1.5) {
+        moveKnot(k);
+        t9LocSet.add(`${locations[9][0]},${locations[9][1]}`);
+      }
+    }
+  }
+}
+
+console.log(t9LocSet.size);
